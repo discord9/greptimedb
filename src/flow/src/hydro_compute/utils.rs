a167b846cbd7acbf595a8de5acd8d48b7a14c2ca
Discord9 <discord9@163.com>
2024-02-19 10:13:02 +0800

    chore: fmt

--- src/flow/src/hydro_compute/utils.rs
+++ src/flow/src/hydro_compute/utils.rs
@@ -86,12 +86,15 @@ impl<K: Ord, V> DiffMap<K, V> {
 }
 
 #[test]
-fn test_diff_map(){
+fn test_diff_map() {
     let mut a = DiffMap::default();
     a.insert(1, 1);
     a.insert(2, 2);
     a.insert(3, 3);
-    assert_eq!(a.gen_diff(0), vec![((1, 1), 0, 1), ((2, 2), 0, 1), ((3, 3), 0, 1)]);
+    assert_eq!(
+        a.gen_diff(0),
+        vec![((1, 1), 0, 1), ((2, 2), 0, 1), ((3, 3), 0, 1)]
+    );
 
     a.remove(&2);
     assert_eq!(a.gen_diff(1), vec![((2, 2), 1, -1)]);
